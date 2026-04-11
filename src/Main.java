7583ced57b7b12bb069eca2d2a88b1838c0d1c0f
euisak <dmldbs4977@naver.com>
2026-04-11 23:12:23 +0900

    Mission01-Java핵심문법&흐름

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+        ArrayList<String> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        int likelionNum;
+        do{
+            System.out.println("저장할 아기사자 수를 5이상 입력해주세요. >>");
+            likelionNum = sc.nextInt();
+            sc.nextLine();
+            if (likelionNum <5 ){
+                System.out.println("[오류] 5 이상을 입력해주세요.");
+            }
+        } while (likelionNum < 5);
+
+        System.out.println("아기사자 이름을 입력해주세요. >>");
+        for (int i = 0; i < likelionNum; i++) {
+            String name = sc.nextLine();
+            list.add(name);
+        }
+
+        System.out.println("아기사자 명단을 최종적으로 출력합니다.");
+        for (int i=0; i<list.size(); i++){
+            System.out.println((i+1) +". " + list.get(i));
+        }
+
+        sc.close();
+
     }
 }
